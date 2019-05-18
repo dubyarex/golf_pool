bcf15157321aec4820f286e35ff3671dc2f421a3
C Miller <caseysgit@gmail.com>
2019-05-18 09:44:02 -0400

    Update auto-refresh.py for Saturday

--- auto-refresh.py
+++ auto-refresh.py
@@ -1,20 +1,22 @@
 #! C:\Anaconda3\envs\py3\python
 
-import os, time, subprocess
+import os
+import time
+import subprocess
 from datetime import datetime
 
 
-stoptime = datetime(2019, 5, 17, 20, 15, 0)
+stoptime = datetime(2019, 5, 18, 20, 15, 0)
 
 while stoptime > datetime.now():
-	print(datetime.now())
-	subprocess.Popen(['C:\\Anaconda3\\envs\\py3\\python.exe', 'C:\\Stuff\\Programming\\golf_pool\\golf_leaderboard.py'])
-	time.sleep(300)
+    print(datetime.now())
+    subprocess.Popen(['C:\\Anaconda3\\envs\\py3\\python.exe', 'C:\\Stuff\\Programming\\golf_pool\\golf_leaderboard.py'])
+    time.sleep(300)
 
 print('''
-	********************************************
-	********************************************
-	************ Auto-refresh ended ************
-	********************************************
-	********************************************
-	''')
+    ********************************************
+    ********************************************
+    ************ Auto-refresh ended ************
+    ********************************************
+    ********************************************
+    ''')
